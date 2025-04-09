2cd12eeccc682bb27407961cbea3c5e92cd102f2
Diego <iwujigoodluck5@gmail.com>
2025-04-09 08:16:36 -0700

    update

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 `use strict`;
+
+document.querySelector('.nav-mobile').addEventListener("click", function () {
+    const menu = document.querySelector('#menu');
+    menu.classList.toggle('h-0');
+    menu.classList.toggle('p-2');
+});
